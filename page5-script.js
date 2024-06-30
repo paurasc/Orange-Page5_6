7dda1f0e84f659dd7949e57002012c67e4563bf6
Pauras Chaudhari <paurasc@gmail.com>
2024-06-30 13:05:04 -0700

    page 5 and 6 updated

--- page5-script.js
+++ page5-script.js
@@ -10,3 +10,11 @@ document.addEventListener("DOMContentLoaded", function() {
         dropdownMenu.style.display = "none";
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const navToggle = document.querySelector('.navbar-toggler');
+    const navMenu = document.querySelector('#page6-nav ul');
+
+    navToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('show');
+    });
+});
